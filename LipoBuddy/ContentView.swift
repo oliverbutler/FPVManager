9a9457a534e7eae45a55e82a09a44debe9b974f6
Oliver Butler <dev@oliverbutler.uk>
2021-02-02 18:13:24 +0000

    Initial Commit

---
+++ LipoBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LipoBuddy
+//
+//  Created by Oliver Butler on 02/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LipoBuddy/LipoBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LipoBuddyApp.swift
+//  LipoBuddy
+//
+//  Created by Oliver Butler on 02/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct LipoBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
